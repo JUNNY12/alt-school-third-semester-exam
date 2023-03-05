e572f1e103ae268dfff04cb858b63df70d0055d4
unknown <juwonemmanuel22@gmail.com>
2023-03-05 16:38:23 +0100

    added link

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,7 +3,7 @@
         <div class="home">
             <h1>Welcome On Board</h1>
             <p>Student Id: ALT/SOE/022/2629</p>
-            <a href="http://">Github Link</a>
+            <a href="https://github.com/JUNNY12/alt-school-third-semester-exam">Github Link</a>
         </div>
     </div>
 </template>
